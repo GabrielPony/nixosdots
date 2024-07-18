68988397c852b4426a122c132d064f6a7daff104
GabrielPony <aoengo@outlook.com>
2024-07-18 14:15:15 +0000

    add chinese input function

--- modules/core/fcitx5.nix
+++ modules/core/fcitx5.nix
@@ -2,11 +2,12 @@
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
+    fcitx.engines = with pkgs.fcitx-engines; [ rime ];
+    fcitx5.enableRimeData= true;
     fcitx5.addons = with pkgs; [
-      rime-data
+      fcitx5-rime
       fcitx5-gtk
       fcitx5-chinese-addons
-      fcitx5-nord
     ];
   };
 }
