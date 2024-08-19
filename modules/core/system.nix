19e061e3ed1c37d0da7a0c4494c40c75fe977ce6
GabrielPony <aoengo@outlook.com>
2024-08-19 14:55:05 +0000

    update time setting

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -27,6 +27,7 @@
   ];
 
   time.timeZone = "Asian/Shanghai";
+  time.hardwareClockInLocalTime = true;
   i18n.defaultLocale = "en_US.UTF-8";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.05";
