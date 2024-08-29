c704a1e437d872bd94c97a88a1fd9f0a354f8066
GabrielPony <aoengo@outlook.com>
2024-08-29 15:58:01 +0000

    setting network of laptop

--- hosts/laptop/addon-configuration.nix
+++ hosts/laptop/addon-configuration.nix
@@ -1,10 +1,11 @@
 { pkgs, inputs, nixpkgs, self, username, host, ... }: 
 {
   networking = {
-    interfaces.enp2s0.ipv4.addresses = [{
-      address = "192.168.7.60";
-      prefixLength = 24;
-    }];
+    interfaces.enp2s0.useDHCP = true;
+    # interfaces.enp2s0.ipv4.addresses = [{
+    #   address = "192.168.7.60";
+    #   prefixLength = 24;
+    # }];
     interfaces.wlo1.ipv4.addresses = [{
       address = "192.168.7.61";
       prefixLength = 24;
