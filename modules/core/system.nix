979d924d371a93554fbc092e45273c8445d24513
GabrielGao <56246210+GabrielPony@users.noreply.github.com>
2024-11-15 09:39:12 +0800

    Update system.nix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -5,7 +5,7 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [ "https://nix-gaming.cachix.org" ];
+      substituters = [  "https://mirrors.ustc.edu.cn/nix-channels/store" "https://nix-gaming.cachix.org" ];
       trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
     };
     gc = {
