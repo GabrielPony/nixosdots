7458e3a02df5d05e47e85d3706ecd5a14e33322e
GabrielPony <aoengo@outlook.com>
2024-11-24 07:06:37 +0000

    update fcitx5

--- modules/home/fuzzel.nix
+++ modules/home/fuzzel.nix
@@ -9,11 +9,11 @@
         line-height = 25;
         fields = "name,generic,comment,categories,filename,keywords";
         terminal = "kitty";
-        prompt = "' ➜  '" ;
+        prompt = "' ➜  '";
         icon-theme = "Papirus-Dark";
         layer = "top";
         lines = 10;
-        width = 35;
+        width = 60;
         horizontal-pad = 25;
         inner-pad = 5;
       };
