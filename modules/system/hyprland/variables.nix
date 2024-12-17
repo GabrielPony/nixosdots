46813b6a0745ff742e068b373b0dc41e7887e625
GabrielPony <aoengo@outlook.com>
2024-12-17 23:04:32 +0800

    update code

--- modules/system/hyprland/variables.nix
+++ modules/system/hyprland/variables.nix
@@ -1,10 +1,11 @@
 { ... }:
 {
   home.sessionVariables = {
+    MOZ_WEBRENDER = "1";
     NIXOS_OZONE_WL = "1";
     __GL_GSYNC_ALLOWED = "0";
     __GL_VRR_ALLOWED = "0";
-    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
     SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
     DISABLE_QT5_COMPAT = "0";
     GDK_BACKEND = "wayland";
@@ -13,7 +14,7 @@
     WLR_DRM_NO_ATOMIC = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_QPA_PLATFORM = "xcb";
+    QT_QPA_PLATFORM = "wayland";
     # QT_QPA_PLATFORMTHEME = "qt5ct";
     # QT_STYLE_OVERRIDE = "kvantum";
     MOZ_ENABLE_WAYLAND = "1";
